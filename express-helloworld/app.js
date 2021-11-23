230f9e8aebf3b2e215f85a79fb491f6edeab4a02
sharmatanish <tanishsharma0209@gmail.com>
2021-11-23 08:59:03 +0000

    added mars

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,9 +2,14 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+  res.send('Hello World to all!\n');
 });
 
+
+app.get('/mars', function (req, res) {
+    res.send('Hello mars!\n');
+  });
+  
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
